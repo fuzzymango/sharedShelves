4cacbb5fb9603c417c026a6e4ac3a98240a02dd2
fuzzymango <46225294+fuzzymango@users.noreply.github.com>
2021-05-02 10:40:17 -0500

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,9 @@
+# import dropboxPath
+# print dropboxPath.get_nuke_setup_path('personal')
+
+import SharedShelves
+# SharedShelves.createShelves(addMenu=True)
+SharedShelves.create_toolbar()
+
+
+# myGizmosMenu = nuke.menu('Nodes').addMenu('IsaacGizmos', icon='C:\Users\isaac.spiegel\Desktop\.nuke backup 4.29\icons')
