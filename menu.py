2e35e361c8571c3619a338ea0720592ab0cce5bb
fuzzymango <46225294+fuzzymango@users.noreply.github.com>
2021-05-07 21:19:52 -0500

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,9 +1,6 @@
-# import dropboxPath
-# print dropboxPath.get_nuke_setup_path('personal')
+
 
 import SharedShelves
-# SharedShelves.createShelves(addMenu=True)
-SharedShelves.create_toolbar()
+SharedShelves.main()
 
 
-# myGizmosMenu = nuke.menu('Nodes').addMenu('IsaacGizmos', icon='C:\Users\isaac.spiegel\Desktop\.nuke backup 4.29\icons')
