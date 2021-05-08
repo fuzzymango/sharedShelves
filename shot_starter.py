9f0411157dfd10cb475389e771abd797769c5c8e
fuzzymango <46225294+fuzzymango@users.noreply.github.com>
2021-05-07 21:50:15 -0500

    Update shot_starter.py

--- shot_starter.py
+++ shot_starter.py
@@ -8,6 +8,8 @@ import sys
 import json
 import re
 
+LUT_NAME = 'blackf_REC_v2.cube'
+
 def get_dropbox_location(account_type):
   info_path = create_dropbox_info_path('LOCALAPPDATA')
   info_dict = get_dictionary_from_path_to_json(info_path)
@@ -26,7 +28,31 @@ def get_dictionary_from_path_to_json(info_path):
   return json.loads(text) 
 
 
+def find_folder(path, folderName):
+  for roots, dirs, files in os.walk(path, topdown=True):
+    if folderName in dirs:
+      folderPath = os.path.join(roots, folderName)
+  if os.path.exists(folderPath):
+    return folderPath
+  return None
 
-def link_lut():
+def find_file_in_directory(path, fileName):
+  fileList = [file for file in os.listdir(path)
+    if os.path.isfile(os.path.join(path, file))]
+
+  return fileList
+
+
+def main():
   dropbox_sync_path = get_dropbox_location('personal')
-  print dropbox_sync_path
+  folderPath = find_folder(dropbox_sync_path, 'lut')
+  file = find_file_in_directory(folderPath, LUT_NAME)
+
+  filePath = os.path.join(folderPath, file[0])
+  return filePath
+
+
+
+lutFile = main().replace(os.sep, '/')
+n = nuke.selectedNode()
+n['file'].setValue(lutFile)
