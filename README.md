62edf7138fb3100246231de50c622bf8812fe457
fuzzymango <ilspiegel21@gmail.com>
2022-08-19 19:29:52 -0400

    Updates for MacOS

--- README.md
+++ README.md
@@ -1,30 +1,44 @@
-# Share Gizmos and ToolSets from Dropbox to Nuke!
+# Sync your Gizmos and ToolSets from Dropbox to Nuke!
+
+The SharedShelves Python script an out-of-the-box pipeline solution for small production studios (or individuals) that enables multiple 
+remote artists to easily share their gizmos, templates, plug-ins, and toolsets with other team members.
+The SharedShelves pipeline allows any artist to share their plug-ins to other team members by simply pasting .gizmo or .nk
+files into a shared Dropbox folder. The SharedShelves script will reference the Dropbox folder and when Nuke launches, 
+load any plug-ins from the folder into a new shelf in the toolbar with the same name as the Dropbox folder. Other artists
+are then able to access the shared tools using Nuke's tab menu or toolbar window.
+
 ## Installation Instructions
-1. Download the "SharedShelves.py" file and place it in your .nuke directory. 
-2. Copy the contents from "menu.py" and paste them in your menu.py file located in your .nuke directory.
-3. Edit the parameters in the SharedShelves() method with your unique Dropbox information (see below) 
+1. Create a folder inside Dropbox and place your tools inside. 
+2. Download the "SharedShelves.py" file and place it in your .nuke directory. 
+3. Copy the contents from "menu.py" and paste them in your menu.py file located in your .nuke directory.
+4. Edit the parameters in the SharedShelves() method with your unique Dropbox information (see below) 
 
-
-**dropbox_tools_folder**: --CASE SENSITIVE-- The name of the folder in your Dropbox containing the gizmos and 
+>**dropbox_tools_folder**: --CASE SENSITIVE-- The name of the folder in your Dropbox containing the gizmos and 
 toolsets you'd like to sync. Change this parameter to exactly match the name of your Dropbox folder. For example,
-if your folder sturcture appears as below on Dropbox. Set the `dropbox_tools_folder` parameter to `sharedNukeTools`.
+if your folder structure appears as below on Dropbox. Set the `dropbox_tools_folder` parameter to `sharedNukeTools`.
 
 ```commandline
-        sharedNukeTools
-            gizmos
-                myCoolGizmo
-                anotherNeatGizmo
-                etc.
-            ToolSets
-                shotTemplate
-                keyerTemplate
-                etc.
+    Dropbox/
+        sharedNukeTools/
+            gizmos/
+                myCoolGizmo.gizmo
+                anotherNeatGizmo.gizmo
+                ...
+            ToolSets/
+                shotTemplate.nk
+                keyerTemplate.nk
+                ...
 ```
 
-**icon**: Change this to the filepath of icon you'd like to appear for the toolbar menu in Nuke. For example: 
-`icon='G:/Dropbox/sharedNukeToolsBETA/icons/sharedToolbar.png'`
+>**icon**: Change this to the filepath of icon you'd like to appear for the toolbar menu in Nuke. For example: 
+`icon='G:/Dropbox/sharedNukeTools/icons/sharedToolbar.png'` Optional
 
-**account_type**: Change this to match the type of Dropbox account that you're using (business, personal, etc.)
+>**account_type**: Change this to match the type of Dropbox account that you're using (personal, business, etc.)
+> The default is `personal`
 
-Using the above examples, my SharedShelves class would look like this: 
-`s = SharedShelves(dropbox_tools_folder='sharedNukeToolsBETA', icon='G:/Dropbox/sharedNukeToolsBETA/icons/sharedToolbar.png', account_type='personal')`
+>Using the above examples, my SharedShelves class would look like this: 
+`s = SharedShelves(dropbox_tools_folder='sharedNukeTools', icon='G:/Dropbox/sharedNukeTools/icons/sharedToolbar.png', account_type='personal')`
+4. Edit the `folder_name` parameter for both `s.sync_gizmos()` and `s.sync_toolsets()` to EXACTLY match (case-sensitive) the name Dropbox folder where your gizmos and toolsets are stored. 
+For example, if your gizmos are stored in `Dropbox/sharedNukeTools/myGizmos`, your sync_gizmos would be `s.sync_gizmos("myGizmos")`. 
+By default, `sync_gizmos()` will look for a folder named "gizmos" and `sync_toolsets()` will look for a folder named "ToolSets".
+5. Launch Nuke!
--- SharedShelves.py
+++ SharedShelves.py
@@ -1,20 +1,22 @@
-'''
+"""
 SharedShelves.py
-version 0.3.1 - 17 August 2022
+19 August 2022
 Isaac Spiegel
 isaacspiegel.com
-'''
+TODO: create a script to add the user's currently selected nodes to the shared Dropbox folder
+"""
 
 import nuke
 import os
 import json
 from pathlib import Path
+import platform
 
 
-class SharedShelves():
+class SharedShelves:
     def __init__(self, dropbox_tools_folder: str = 'sharedNukeTools', icon: str = None,
                  account_type: str = 'personal') -> None:
-        '''
+        """
         __init__
         :param dropbox_tools_folder: str
             The name of the folder where the shared tools are stored
@@ -22,22 +24,33 @@ class SharedShelves():
             The location of the icon for the toolbar menu
         :param account_type: str
             The type of Dropbox account the user is in possession of
-        '''
+        """
+        self.VALID_ICON_FILE_TYPES = ['.png', '.jpg', '.jpeg']
+        self.VALID_GIZMO_FILE_TYPES = ['.gizmo']
+        self.VALID_TOOLSET_FILE_TYPES = ['.gizmo', '.nk']
+        self.USER_OS = platform.system()
+
         self.account_type = account_type
         self.dropbox_tools_folder = dropbox_tools_folder
         self.icon = icon
         self.dropbox_install_location = self.find_dropbox_install_directory()
-        self.dropbox_tools_folder_location = self.find_folder_in_dropbox(self.dropbox_install_location, self.dropbox_tools_folder)
-        self.VALID_ICON_FILE_TYPES = ['.png', '.jpg', '.jpeg']
-        self.VALID_GIZMO_FILE_TYPES = ['.gizmo']
-        self.VALID_TOOLSET_FILE_TYPES = ['.gizmo', '.nk']
+        self.dropbox_tools_folder_location = self.find_folder_in_dropbox(self.dropbox_install_location,
+                                                                         self.dropbox_tools_folder)
 
-    def find_dropbox_install_directory(self) -> str:
-        '''
+    def find_dropbox_install_directory(self) -> str or None:
+        """
         Locates the filepath of the Dropbox install on the user's computer
-        :return:
-        '''
-        dropbox_info_path = Path(Path(os.environ['LOCALAPPDATA']) / Path(r'Dropbox\info.json'))
+        :return: str
+            The filepath to the Dropbox install directory as a string
+        """
+        dropbox_info_path = None
+        if self.USER_OS == 'Windows':
+            try:
+                dropbox_info_path = Path(Path(os.environ['LOCALAPPDATA']) / Path(r'Dropbox\info.json'))
+            except FileNotFoundError:
+                dropbox_info_path = Path(Path(os.environ['APPDATA']) / Path(r'Dropbox\info.json'))
+        elif self.USER_OS == 'Darwin':
+            dropbox_info_path = Path(Path(os.path.expanduser('~')) / Path(r'.dropbox/info.json'))
         if not dropbox_info_path.exists():
             return None
 
@@ -47,8 +60,9 @@ class SharedShelves():
 
         return dropbox_info_content[self.account_type]['path']
 
-    def find_folder_in_dropbox(self, install_directory: str, folder_name: str) -> Path:
-        '''
+    @staticmethod
+    def find_folder_in_dropbox(install_directory: str, folder_name: str) -> Path or None:
+        """
         Locates a named folder inside a directory and returns the path to that folder.
         :param folder_name: str
             The name of the folder to find
@@ -56,7 +70,7 @@ class SharedShelves():
             The directory to search for the folder_name
         :return:
             The path to the folder
-        '''
+        """
         if not install_directory: return None
 
         install_directory = Path(install_directory)
@@ -65,31 +79,40 @@ class SharedShelves():
                 return folder
         return None
 
-    def _verify(self) -> bool:
+    def _verify(self, sync_folder='folder_name') -> bool:
+        """
+        A check to see if the Dropbox install and folder have been correctly located. Opens a Nuke message window
+        notifiying the user as to what's missing.
+        :return: bool
+            True if if it's all good and False if errors.
+        """
         if self.dropbox_install_location is None:
             nuke.message(f"Unable to locate info.json in the Dropbox install directory."
-                         f"\nDropbox tools will not be synced.")
+                         f"\nDropbox folder \"{sync_folder}\" will not be synced.")
             return False
         if self.dropbox_tools_folder_location is None:
             nuke.message(
-                f"Unable to locate folder {self.dropbox_tools_folder} in directory {self.dropbox_install_location}."
-                f"\nDropbox tools will not be synced.")
+                f"Unable to locate folder \"{self.dropbox_tools_folder}\" in directory \"{self.dropbox_install_location}\"."
+                f"\nDropbox folder \"{sync_folder}\" will not be synced.")
             return False
         return True
 
-
     def sync_gizmos(self, folder_name: str = 'gizmos') -> None:
-        '''
+        """
         This is the primary method for syncing any gizmos found on Dropbox with a toolbar in Nuke. A new toolbar is
         created to house the loaded tools from Dropbox. This method expects a folder named 'gizmos' to exist
         somewhere inside the user-defined Dropbox folder.
         :return:
             None
-        '''
-        if not self._verify(): return
+        """
+        if not self._verify(folder_name):
+            return
 
         toolbar = nuke.toolbar('Nodes').addMenu(self.dropbox_tools_folder, icon=self.icon)
         path_to_gizmos = Path(self.dropbox_tools_folder_location / Path(folder_name))
+        if not path_to_gizmos.exists():
+            self._folder_not_found_error(path_to_gizmos, folder_name)
+
         nuke.pluginAddPath(str(path_to_gizmos), False)
 
         for folder in path_to_gizmos.glob('**/'):
@@ -109,7 +132,7 @@ class SharedShelves():
 
     @staticmethod
     def retrieve_relative_path(file: Path, relative_to: str = '.nuke') -> str:
-        '''
+        """
         Takes in a file path and a reference folder and outputs a filepath relative to the reference folder
         :param file: Path
             The larger filepath containing the 'relative_to' folder
@@ -117,10 +140,13 @@ class SharedShelves():
             Where the truncated filepath will start
         :return: str
             A truncated filpath relative to the 'relative_to' folder
-        '''
+        """
         relative_path_start = file.parts.index(relative_to) + 1
         relative_path_parts = file.parts[relative_path_start:]
         relative_path = ''
+
+        if len(relative_path_parts) == 1:
+            return file.stem
         for index, part in enumerate(relative_path_parts):
             if index == 0:
                 relative_path = f'{part}'
@@ -132,7 +158,7 @@ class SharedShelves():
         return relative_path
 
     def fetch_icons(self, folder: Path) -> dict:
-        '''
+        """
         Creates a dictionary of all files in the specified directory that contain an extension defined in
         VALID_ICON_FILE_TYPES
         :param folder: Path
@@ -140,7 +166,7 @@ class SharedShelves():
         :return: icons: dict
             A dictionary containing the stem of the image file (no extension) for the keys and the name of the image
             file (with extension) for the values.
-        '''
+        """
         icons = {}
         for file in folder.glob('*.*'):
             if file.suffix in self.VALID_ICON_FILE_TYPES:
@@ -148,18 +174,22 @@ class SharedShelves():
         return icons
 
     def sync_toolsets(self, folder_name: str = 'ToolSets') -> None:
-        '''
+        """
         This is the primary method for syncing any toolsets found on Dropbox with Nuke's "ToolSets" folder. This method
         expects a folder named "ToolSets" (case sensitive) to exist somewhere in the user-defined Dropbox folder
         structure.
         :param folder_name: str
             The name of the Dropbox folder this method should load toolsets from. "ToolSets" by default.
         :return:
-        '''
-        if not self._verify(): return
+        """
+        if not self._verify(folder_name):
+            return
 
         toolsets_toolbar = nuke.toolbar('Nodes').menu('ToolSets')
         path_to_toolbar_items = Path(self.dropbox_tools_folder_location / Path(folder_name))
+        if not path_to_toolbar_items.exists():
+            self._folder_not_found_error(path_to_toolbar_items, folder_name)
+
         nuke.pluginAddPath(str(path_to_toolbar_items), False)
 
         for folder in path_to_toolbar_items.glob('**/'):
@@ -169,10 +199,24 @@ class SharedShelves():
 
             for file in folder.glob('*.*'):
                 if file.suffix in self.VALID_TOOLSET_FILE_TYPES:
-                    shelf_name = file.stem
+                    shelf_name = self.retrieve_relative_path(file, relative_to=folder_name)
                     create_command = f"nuke.createNode('{file.name}')"
                     if file.stem in icons:
                         icon_title = icons[file.stem]
                     else:
                         icon_title = ''
                     toolsets_toolbar.addCommand(shelf_name, create_command, icon=icon_title)
+
+    @staticmethod
+    def _folder_not_found_error(folder_path: Path, folder_name: str) -> None:
+        """
+        Prompts the user with a message window informing them which folder can't be found.
+        :param folder_path: Path
+            The filepath where the method was looking
+        :param folder_name: str
+            The name of the folder the method was hoping to find
+        :return: None
+        """
+        nuke.message(f"Unable to locate the folder \"{folder_name}\" in location: \"{folder_path}\"\n"
+                     f"Check that the folder name provided EXACTLY matches the name of the folder found on Dropbox"
+                     f"(case-sensitive)")
